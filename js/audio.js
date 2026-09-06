8e895d2b98957b2f66ae05cb8fe1eb70647d1f2a
Andrew <staticjuice@Andrew.local>
2026-09-07 02:57:14 +0900

    Fourth massive wave of optimisations

--- js/audio.js
+++ js/audio.js
@@ -1,28 +1,35 @@
-const tracks = window.TRACKS,
-    buttons = document.querySelectorAll('.sample-grid > .sample-button'),
-    artist = document.getElementById('sample-artist'),
-    track = document.getElementById('sample-track'),
-    audio = tracks.map(t => Object.assign(new Audio(t.path), { preload: 'auto' }));
+const article = document.querySelector('article'),
+    name = location.pathname.split('/').pop().slice(0, -5),
+    sample = new URL(`../samples/${document.body.className}/${name}`, document.currentScript.src).href;
 
-let current, selected;
+document.body.style.setProperty('--bg', document.body.style.getPropertyValue('--bg') || `url(${sample.replace('/samples/', '/images/')}.jpg)`);
+
+article.insertAdjacentHTML('beforeend', `
+    <div>${tracks.map((_, i) => `<div>${i + 1}</div>`).join('')}</div>
+    <span>Artist:<span></span></span>
+    <span>Track:<span></span></span>
+    <div><svg viewBox="0 -3 24 24"><rect x="4" y="4" width="16" height="16" rx="2" fill="currentColor"/></svg></div>
+`);
+
+const [artist, track] = article.querySelectorAll(':scope > span > span');
+
+let active;
 
 const stop = () => {
-    if (current) current.pause(), current.currentTime = 0;
-    selected?.classList.remove('is-playing');
-    current = selected = null;
+    if (active != null) audio[active].pause(), audio[active].currentTime = 0, article.querySelector('[data-playing]')?.removeAttribute('data-playing');
+    active = null;
     artist.textContent = track.textContent = '';
 };
 
-buttons.forEach((button, i) => {
-    audio[i].onended = stop;
+const audio = tracks.map((_, i) => Object.assign(new Audio(`${sample}${i + 1}.ogg`), { onended: stop }));
 
-    button.onclick = () => {
-        stop();
-        (current = audio[i]).play();
-        (selected = button).classList.add('is-playing');
-        artist.textContent = tracks[i].artist;
-        track.textContent = tracks[i].track;
-    };
-});
+article.onclick = ({ target: button }) => {
+    if (button.tagName !== 'DIV' || button.parentElement?.parentElement !== article) return;
 
-document.querySelector('.sample-stop').onclick = stop;
+    stop();
+    audio[active = button.textContent - 1].play();
+    button.setAttribute('data-playing', '');
+    [artist.textContent, track.textContent] = tracks[active];
+};
+
+article.lastElementChild.onclick = stop;
