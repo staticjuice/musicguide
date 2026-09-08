371f777c4cf426cd1eddcd5bfd1832d9e543426c
Andrew <staticjuice@Andrew.local>
2026-09-09 03:07:28 +0900

    Make the audio samples playable by using the number keys and implement a basic page transition crossfade

--- js/audio.js
+++ js/audio.js
@@ -11,7 +11,8 @@ article.insertAdjacentHTML('beforeend', `
     <div><svg viewBox="0 0 24 24"><rect x="4" y="4" width="16" height="16" rx="2" fill="currentColor"/></svg></div>
 `);
 
-const [artist, track] = article.querySelectorAll(':scope > span > span');
+const buttons = article.querySelectorAll(':scope > div:first-of-type > div'),
+    [artist, track] = article.querySelectorAll(':scope > span > span');
 
 let active;
 
@@ -23,13 +24,26 @@ const stop = () => {
 
 const audio = tracks.map((_, i) => Object.assign(new Audio(`${sample}${i + 1}.ogg`), { onended: stop }));
 
+const play = i => {
+    if (!audio[i]) return;
+
+    stop();
+    (active = audio[i]).play();
+    buttons[i].setAttribute('data-playing', '');
+    [artist.textContent, track.textContent] = tracks[i];
+};
+
 article.onclick = ({ target }) => {
     const button = target.closest('div');
+
     if (button === article.lastElementChild) return stop();
     if (button?.parentElement?.parentElement !== article) return;
-    stop();
-    const i = button.textContent - 1;
-    (active = audio[i]).play();
-    button.setAttribute('data-playing', '');
-    [artist.textContent, track.textContent] = tracks[i];
+
+    play(button.textContent - 1);
+};
+
+document.onkeydown = ({ key, repeat }) => {
+    if (repeat) return;
+    if (key === 'Escape' || key === '0') return stop();
+    if (key >= '1' && key <= '9') play(Number(key) - 1);
 };
