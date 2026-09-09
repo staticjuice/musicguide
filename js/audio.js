952a7e183448955e5c9c421623918a8da53a6bf9
Andrew <staticjuice@Andrew.local>
2026-09-09 10:55:53 +0900

    Implement a moving inset ring as a playback progress indicator to the sample buttons

--- js/audio.js
+++ js/audio.js
@@ -2,10 +2,14 @@ const article = document.querySelector('article'),
     name = location.pathname.split('/').pop().slice(0, -5),
     sample = new URL(`../samples/${document.body.className}/${name}`, document.currentScript.src).href;
 
-document.body.style.setProperty('--bg', document.body.style.getPropertyValue('--bg') || `url(${sample.replace('/samples/', '/images/')}.jpg)`);
+document.body.style.setProperty(
+    '--bg',
+    document.body.style.getPropertyValue('--bg') ||
+    `url(${sample.replace('/samples/', '/images/')}.jpg)`
+);
 
 article.insertAdjacentHTML('beforeend', `
-    <div>${tracks.map((_, i) => `<div>${i + 1}</div>`).join('')}</div>
+    <div>${tracks.map((_, i) => `<div>${i + 1}<svg viewBox="0 0 100 125"><path d="M50 5 H85 Q95 5 95 15 V110 Q95 120 85 120 H15 Q5 120 5 110 V15 Q5 5 15 5 H50"/><path pathLength="100" d="M50 5 H85 Q95 5 95 15 V110 Q95 120 85 120 H15 Q5 120 5 110 V15 Q5 5 15 5 H50"/></svg></div>`).join('')}</div>
     <span>Artist:<span></span></span>
     <span>Track:<span></span></span>
     <div><svg viewBox="0 0 24 24"><rect x="4" y="4" width="16" height="16" rx="2" fill="currentColor"/></svg></div>
@@ -14,23 +18,55 @@ article.insertAdjacentHTML('beforeend', `
 const buttons = article.querySelectorAll(':scope > div:first-of-type > div'),
     [artist, track] = article.querySelectorAll(':scope > span > span');
 
-let active;
+let active, activeButton, frame;
 
 const stop = () => {
-    if (active) active.pause(), active.currentTime = 0, article.querySelector('[data-playing]')?.removeAttribute('data-playing');
-    active = null;
+    if (active) {
+        active.pause();
+        active.currentTime = 0;
+        activeButton.removeAttribute('data-playing');
+        activeButton.style.removeProperty('--progress');
+        cancelAnimationFrame(frame);
+    }
+
+    active = activeButton = null;
     artist.textContent = track.textContent = '';
 };
 
-const audio = tracks.map((_, i) => Object.assign(new Audio(`${sample}${i + 1}.ogg`), { onended: stop }));
+const audio = tracks.map((_, i) =>
+    Object.assign(
+        new Audio(`${sample}${i + 1}.ogg`),
+        { onended: stop }
+    )
+);
+
+const draw = () => {
+    if (!active) return;
+
+    if (Number.isFinite(active.duration))
+        activeButton.style.setProperty(
+        '--progress',
+        active.currentTime / active.duration * 100
+    );
+
+    frame = requestAnimationFrame(draw);
+};
 
 const play = i => {
     if (!audio[i]) return;
 
     stop();
-    (active = audio[i]).play();
-    buttons[i].setAttribute('data-playing', '');
+
+    active = audio[i];
+    activeButton = buttons[i];
+
+    activeButton.setAttribute('data-playing', '');
+    activeButton.style.setProperty('--progress', 0);
+
     [artist.textContent, track.textContent] = tracks[i];
+
+    active.play();
+    frame = requestAnimationFrame(draw);
 };
 
 article.onclick = ({ target }) => {
